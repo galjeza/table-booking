a0546e75a85393cdbae8ff00bf81a3d5560f6ec0
Simon_Plazar <simon.plazar@gmail.com>
2022-11-14 22:01:23 +0100

    add prisma

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+prisma
 
 export default function Home() {
   return (
