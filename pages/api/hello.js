7bb1996e89bbc44acaf996c8d8741b65fdb34e4d
galjeza <gal.jeza@protonmail.com>
2022-11-14 22:49:40 +0100

    setup daisyui, prettier

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from '@prisma/client';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const prisma = new PrismaClient();
+
+export default async (req, res) => {
+  const data = req.body;
+  try {
+    const result = await prisma.user.findMany();
+    res.status(200).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(403).json({ err: 'Error occured.' });
+  }
+};
