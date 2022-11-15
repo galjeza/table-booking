3b960fa42d953c76ff8805beb707b190cc71d5f9
galjeza <gal.jeza@protonmail.com>
2022-11-15 18:46:27 +0100

    format code, remove unused imports, remove comments

--- app/components/ContactSales.js
+++ app/components/ContactSales.js
@@ -1,215 +1,251 @@
-/*
-  This example requires Tailwind CSS v2.0+
-
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-"use client";
-import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+'use client';
+import { useState } from 'react';
+import { Switch } from '@headlessui/react';
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function ContactSales() {
-    const [agreed, setAgreed] = useState(false)
+  const [agreed, setAgreed] = useState(false);
 
-    return (
-        <div id={"contact"} className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
-            <div className="relative max-w-xl mx-auto">
-                <svg
-                    className="absolute left-full transform translate-x-1/2"
-                    width={404}
-                    height={404}
-                    fill="none"
-                    viewBox="0 0 404 404"
-                    aria-hidden="true"
-                >
-                    <defs>
-                        <pattern
-                            id="85737c0e-0916-41d7-917f-596dc7edfa27"
-                            x={0}
-                            y={0}
-                            width={20}
-                            height={20}
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-                </svg>
-                <svg
-                    className="absolute right-full bottom-0 transform -translate-x-1/2"
-                    width={404}
-                    height={404}
-                    fill="none"
-                    viewBox="0 0 404 404"
-                    aria-hidden="true"
-                >
-                    <defs>
-                        <pattern
-                            id="85737c0e-0916-41d7-917f-596dc7edfa27"
-                            x={0}
-                            y={0}
-                            width={20}
-                            height={20}
-                            patternUnits="userSpaceOnUse"
-                        >
-                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                        </pattern>
-                    </defs>
-                    <rect width={404} height={404} fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
-                </svg>
-                <div className="text-center">
-                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">Kontaktirajte nas</h2>
-                    <p className="mt-4 text-lg leading-6 text-gray-500">
-                        Odprti smo za vsa vprašanja in komentarje.
-                    </p>
-                </div>
-                <div className="mt-12">
-                    <form action="#" method="POST" className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-                        <div>
-                            <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                                Ime
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    type="text"
-                                    name="first-name"
-                                    id="first-name"
-                                    autoComplete="given-name"
-                                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                                Priimek
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    type="text"
-                                    name="last-name"
-                                    id="last-name"
-                                    autoComplete="family-name"
-                                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="company" className="block text-sm font-medium text-gray-700">
-                                Podjetje
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    type="text"
-                                    name="company"
-                                    id="company"
-                                    autoComplete="organization"
-                                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                                Email
-                            </label>
-                            <div className="mt-1">
-                                <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    autoComplete="email"
-                                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="phone-number" className="block text-sm font-medium text-gray-700">
-                                Telefon
-                            </label>
-                            <div className="mt-1 relative rounded-md shadow-sm">
-
-                                <input
-                                    type="text"
-                                    name="phone-number"
-                                    id="phone-number"
-                                    autoComplete="tel"
-                                    className="py-3 px-4 block w-full  focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                                    placeholder="031 123 456"
-                                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block text-sm font-medium text-gray-700">
-                                Sporočilo
-                            </label>
-                            <div className="mt-1">
-                <textarea
-                    id="message"
-                    name="message"
-                    rows={4}
-                    className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
-                    defaultValue={''}
-                />
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <div className="flex items-start">
-                                <div className="flex-shrink-0">
-                                    <Switch
-                                        checked={agreed}
-                                        onChange={setAgreed}
-                                        className={classNames(
-                                            agreed ? 'bg-indigo-600' : 'bg-gray-200',
-                                            'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                                        )}
-                                    >
-                                        <span className="sr-only">Agree to policies</span>
-                                        <span
-                                            aria-hidden="true"
-                                            className={classNames(
-                                                agreed ? 'translate-x-5' : 'translate-x-0',
-                                                'inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
-                                            )}
-                                        />
-                                    </Switch>
-                                </div>
-                                <div className="ml-3">
-                                    <p className="text-base text-gray-500">
-                                        Strinjam se z{' '}
-                                        <a href="#" className="font-medium text-gray-700 underline">
-                                            politiko zasebnosti
-                                        </a>{' '}
-
-                                        .
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="sm:col-span-2">
-                            <button
-                                type="submit"
-                                className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                                Pošlji sporočilo
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+  return (
+    <div
+      id={'contact'}
+      className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24"
+    >
+      <div className="relative max-w-xl mx-auto">
+        <svg
+          className="absolute left-full transform translate-x-1/2"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
+        </svg>
+        <svg
+          className="absolute right-full bottom-0 transform -translate-x-1/2"
+          width={404}
+          height={404}
+          fill="none"
+          viewBox="0 0 404 404"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="85737c0e-0916-41d7-917f-596dc7edfa27"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x={0}
+                y={0}
+                width={4}
+                height={4}
+                className="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width={404}
+            height={404}
+            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+          />
+        </svg>
+        <div className="text-center">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Kontaktirajte nas
+          </h2>
+          <p className="mt-4 text-lg leading-6 text-gray-500">
+            Odprti smo za vsa vprašanja in komentarje.
+          </p>
         </div>
-    )
+        <div className="mt-12">
+          <form
+            action="#"
+            method="POST"
+            className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+          >
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Ime
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Priimek
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Podjetje
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="company"
+                  id="company"
+                  autoComplete="organization"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Telefon
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <input
+                  type="text"
+                  name="phone-number"
+                  id="phone-number"
+                  autoComplete="tel"
+                  className="py-3 px-4 block w-full  focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                  placeholder="031 123 456"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Sporočilo
+              </label>
+              <div className="mt-1">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="py-3 px-4 block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
+                  defaultValue={''}
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <div className="flex items-start">
+                <div className="flex-shrink-0">
+                  <Switch
+                    checked={agreed}
+                    onChange={setAgreed}
+                    className={classNames(
+                      agreed ? 'bg-indigo-600' : 'bg-gray-200',
+                      'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                    )}
+                  >
+                    <span className="sr-only">
+                      Agree to policies
+                    </span>
+                    <span
+                      aria-hidden="true"
+                      className={classNames(
+                        agreed ? 'translate-x-5' : 'translate-x-0',
+                        'inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+                      )}
+                    />
+                  </Switch>
+                </div>
+                <div className="ml-3">
+                  <p className="text-base text-gray-500">
+                    Strinjam se z{' '}
+                    <a
+                      href="#"
+                      className="font-medium text-gray-700 underline"
+                    >
+                      politiko zasebnosti
+                    </a>{' '}
+                    .
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <button
+                type="submit"
+                className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Pošlji sporočilo
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/components/Newsletter.js
+++ app/components/Newsletter.js
@@ -1,45 +1,48 @@
-"use client";
+'use client';
 export default function Example() {
-    return (
-        <div className="bg-white">
-            <div className="max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:py-32 lg:px-8 lg:flex lg:items-center">
-                <div className="lg:w-0 lg:flex-1">
-                    <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">Naroči se na naše novice</h2>
-                    <p className="mt-3 max-w-3xl text-lg text-gray-500">
-                        Zaupaj nam tvoj e-poštni naslov, da boš obveščen o novostih v orodju in promocijskih ponudbah.
-                    </p>
-                </div>
-                <div className="mt-8 lg:mt-0 lg:ml-8">
-                    <form className="sm:flex">
-                        <label htmlFor="email-address" className="sr-only">
-                            Email address
-                        </label>
-                        <input
-                            id="email-address"
-                            name="email-address"
-                            type="email"
-                            autoComplete="email"
-                            required
-                            className="w-full px-5 py-3 border border-gray-300 shadow-sm placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs rounded-md"
-                            placeholder="Vnesi svoj e-poštni naslov"
-                        />
-                        <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-                            <button
-                                type="submit"
-                                className="w-full flex items-center justify-center py-3 px-5 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                            >
-                                Naroči se
-                            </button>
-                        </div>
-                    </form>
-                    <p className="mt-3 text-sm text-gray-500">
-                        Mar nam je za varnost vaših podatkov. Preberi našo{' '}
-                        <a href="#" className="font-medium underline">
-                            Politiko zasebnosti
-                        </a>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-24 px-4 sm:px-6 lg:py-32 lg:px-8 lg:flex lg:items-center">
+        <div className="lg:w-0 lg:flex-1">
+          <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+            Naroči se na naše novice
+          </h2>
+          <p className="mt-3 max-w-3xl text-lg text-gray-500">
+            Zaupaj nam tvoj e-poštni naslov, da boš obveščen o
+            novostih v orodju in promocijskih ponudbah.
+          </p>
         </div>
-    )
+        <div className="mt-8 lg:mt-0 lg:ml-8">
+          <form className="sm:flex">
+            <label htmlFor="email-address" className="sr-only">
+              Email address
+            </label>
+            <input
+              id="email-address"
+              name="email-address"
+              type="email"
+              autoComplete="email"
+              required
+              className="w-full px-5 py-3 border border-gray-300 shadow-sm placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs rounded-md"
+              placeholder="Vnesi svoj e-poštni naslov"
+            />
+            <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+              <button
+                type="submit"
+                className="w-full flex items-center justify-center py-3 px-5 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Naroči se
+              </button>
+            </div>
+          </form>
+          <p className="mt-3 text-sm text-gray-500">
+            Mar nam je za varnost vaših podatkov. Preberi našo{' '}
+            <a href="#" className="font-medium underline">
+              Politiko zasebnosti
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
