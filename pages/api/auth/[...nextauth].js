aff229ee71549bce8b3bd89887cd5ee662900e60
galjeza <gal.jeza@protonmail.com>
2022-11-30 20:24:51 +0100

    :(

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,4 +1,4 @@
-import NextAuth, from "next-auth";
+import NextAuth from "next-auth";
 import CredentialsProvider, from "next-auth/providers/credentials";
 import {PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient()
