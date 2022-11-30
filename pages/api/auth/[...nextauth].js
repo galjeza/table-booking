bc361cff2c9ee4cfccb74293a0e666ad4f273fde
galjeza <gal.jeza@protonmail.com>
2022-11-30 20:15:16 +0100

    remove comments

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,13 +6,7 @@ const authOptions = {
 
     providers: [
         CredentialsProvider({
-            // The name to display on the sign in form (e.g. "Sign in with...")
             name: "Credentials",
-            // `credentials` is used to generate a form on the sign in page.
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-
             async authorize(credentials, req) {
                 // Add logic here to look up the user from the credentials supplied
                 const user = await prisma.user.findUnique({
