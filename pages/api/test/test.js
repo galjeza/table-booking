4b9f6249511deb11276b57fb41b93740c98b1959
galjeza <gal.jeza@protonmail.com>
2022-12-04 19:54:40 +0100

    ...

--- pages/api/test/test.js
+++ pages/api/test/test.js
@@ -3,12 +3,10 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export default async (req, res) => {
-  const data = req.body;
   try {
     const result = await prisma.user.findMany();
     res.status(200).json(result);
   } catch (error) {
-
     res.status(403).json({ error: error.message });
   }
 };
