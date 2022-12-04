15fe3c66807271ff6f8cf626fae0669cb85ef81c
galjeza <gal.jeza@protonmail.com>
2022-12-04 19:38:08 +0100

    testing api

--- pages/api/test/test.js
+++ pages/api/test/test.js
@@ -7,8 +7,8 @@ export default async (req, res) => {
   try {
     const result = await prisma.user.findMany();
     res.status(200).json(result);
-  } catch (err) {
-    console.log(err);
-    res.status(403).json({ err: 'Error occured.' });
+  } catch (error) {
+
+    res.status(403).json({ error: error.message });
   }
 };
