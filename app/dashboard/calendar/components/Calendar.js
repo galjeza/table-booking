ffb14cb5551f0324c7753dbb3bbff2f949ac06c2
galjeza <gal.jeza@protonmail.com>
2022-12-14 22:26:07 +0100

    refactor calendar

---
+++ app/dashboard/calendar/components/Calendar.js
@@ -0,0 +1,85 @@
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon
+} from '@heroicons/react/solid';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export default function Calendar({ days }) {
+  return (
+    <div className="mt-10 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 lg:mt-9 xl:col-start-9">
+      <div className="flex items-center text-gray-900">
+        <button
+          type="button"
+          className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+        >
+          <span className="sr-only">Prejšnji mesec</span>
+          <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+        </button>
+        <div className="flex-auto font-semibold">December</div>
+        <button
+          type="button"
+          className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+        >
+          <span className="sr-only">Naslednji mesec</span>
+          <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+        </button>
+      </div>
+      <div className="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
+        <div>P</div>
+        <div>T</div>
+        <div>S</div>
+        <div>Č</div>
+        <div>P</div>
+        <div>S</div>
+        <div>N</div>
+      </div>
+      <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
+        {days.map((day, dayIdx) => (
+          <button
+            key={day.date}
+            type="button"
+            className={classNames(
+              'py-1.5 hover:bg-gray-100 focus:z-10',
+              day.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
+              (day.isSelected || day.isToday) && 'font-semibold',
+              day.isSelected && 'text-white',
+              !day.isSelected &&
+                day.isCurrentMonth &&
+                !day.isToday &&
+                'text-gray-900',
+              !day.isSelected &&
+                !day.isCurrentMonth &&
+                !day.isToday &&
+                'text-gray-400',
+              day.isToday && !day.isSelected && 'text-indigo-600',
+              dayIdx === 0 && 'rounded-tl-lg',
+              dayIdx === 6 && 'rounded-tr-lg',
+              dayIdx === days.length - 7 && 'rounded-bl-lg',
+              dayIdx === days.length - 1 && 'rounded-br-lg'
+            )}
+          >
+            <time
+              dateTime={day.date}
+              className={classNames(
+                'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
+                day.isSelected && day.isToday && 'bg-indigo-600',
+                day.isSelected && !day.isToday && 'bg-gray-900'
+              )}
+            >
+              {day.date.split('-').pop().replace(/^0/, '')}
+            </time>
+          </button>
+        ))}
+      </div>
+      <button
+        type="button"
+        className="focus:outline-none mt-8 w-full rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        Dodaj rezervacijo
+      </button>
+    </div>
+  );
+}
--- app/dashboard/calendar/page.js
+++ app/dashboard/calendar/page.js
@@ -1,14 +1,6 @@
 'use client';
-import { Fragment } from 'react';
-import {
-  CalendarIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  DotsHorizontalIcon,
-  LocationMarkerIcon,
-  UsersIcon
-} from '@heroicons/react/solid';
-import { Menu, Transition } from '@headlessui/react';
+import EventListItem from './components/EventListItem';
+import Calendar from './components/Calendar';
 
 const reservations = [
   {
@@ -91,10 +83,6 @@ const days = [
   { date: '2022-02-06' }
 ];
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
-
 export default function Example() {
   return (
     <div>
@@ -102,183 +90,10 @@ export default function Example() {
         Rezervacije za datum: 12.12.2022
       </h2>
       <div className="lg:grid lg:grid-cols-12 lg:gap-x-16">
-        <div className="mt-10 text-center lg:col-start-8 lg:col-end-13 lg:row-start-1 lg:mt-9 xl:col-start-9">
-          <div className="flex items-center text-gray-900">
-            <button
-              type="button"
-              className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">Previous month</span>
-              <ChevronLeftIcon
-                className="h-5 w-5"
-                aria-hidden="true"
-              />
-            </button>
-            <div className="flex-auto font-semibold">December</div>
-            <button
-              type="button"
-              className="-m-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">Next month</span>
-              <ChevronRightIcon
-                className="h-5 w-5"
-                aria-hidden="true"
-              />
-            </button>
-          </div>
-          <div className="mt-6 grid grid-cols-7 text-xs leading-6 text-gray-500">
-            <div>M</div>
-            <div>T</div>
-            <div>W</div>
-            <div>T</div>
-            <div>F</div>
-            <div>S</div>
-            <div>S</div>
-          </div>
-          <div className="isolate mt-2 grid grid-cols-7 gap-px rounded-lg bg-gray-200 text-sm shadow ring-1 ring-gray-200">
-            {days.map((day, dayIdx) => (
-              <button
-                key={day.date}
-                type="button"
-                className={classNames(
-                  'py-1.5 hover:bg-gray-100 focus:z-10',
-                  day.isCurrentMonth ? 'bg-white' : 'bg-gray-50',
-                  (day.isSelected || day.isToday) && 'font-semibold',
-                  day.isSelected && 'text-white',
-                  !day.isSelected &&
-                    day.isCurrentMonth &&
-                    !day.isToday &&
-                    'text-gray-900',
-                  !day.isSelected &&
-                    !day.isCurrentMonth &&
-                    !day.isToday &&
-                    'text-gray-400',
-                  day.isToday &&
-                    !day.isSelected &&
-                    'text-indigo-600',
-                  dayIdx === 0 && 'rounded-tl-lg',
-                  dayIdx === 6 && 'rounded-tr-lg',
-                  dayIdx === days.length - 7 && 'rounded-bl-lg',
-                  dayIdx === days.length - 1 && 'rounded-br-lg'
-                )}
-              >
-                <time
-                  dateTime={day.date}
-                  className={classNames(
-                    'mx-auto flex h-7 w-7 items-center justify-center rounded-full',
-                    day.isSelected && day.isToday && 'bg-indigo-600',
-                    day.isSelected && !day.isToday && 'bg-gray-900'
-                  )}
-                >
-                  {day.date.split('-').pop().replace(/^0/, '')}
-                </time>
-              </button>
-            ))}
-          </div>
-          <button
-            type="button"
-            className="focus:outline-none mt-8 w-full rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Dodaj rezervacijo
-          </button>
-        </div>
+        <Calendar days={days} />
         <ol className="mt-4 divide-y divide-gray-100 text-sm leading-6 lg:col-span-7 xl:col-span-8">
           {reservations.map(reservation => (
-            <li
-              key={reservation.id}
-              className="relative flex space-x-6 py-6 xl:static"
-            >
-              <div className="flex-auto">
-                <h3 className="pr-10 font-semibold text-gray-900 xl:pr-0">
-                  {reservation.name}
-                </h3>
-                <dl className="mt-2 flex flex-col text-gray-500 xl:flex-row">
-                  <div className="flex items-start space-x-3">
-                    <dt className="mt-0.5">
-                      <span className="sr-only">Date</span>
-                      <CalendarIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </dt>
-                    <dd>
-                      <time dateTime={reservation.datetime}>
-                        {reservation.date} ob {reservation.time}
-                      </time>
-                    </dd>
-                  </div>
-                  <div className="mt-2 flex items-start space-x-3 xl:mt-0 xl:ml-3.5 xl:border-l xl:border-gray-400 xl:border-opacity-50 xl:pl-3.5">
-                    <dt className="mt-0.5">
-                      <span className="sr-only">Location</span>
-                      <UsersIcon
-                        className="h-5 w-5 text-gray-400"
-                        aria-hidden="true"
-                      />
-                    </dt>
-                    <dd>{reservation.location}</dd>
-                  </div>
-                </dl>
-              </div>
-              <Menu
-                as="div"
-                className="absolute top-6 right-0 xl:relative xl:top-auto xl:right-auto xl:self-center"
-              >
-                <div>
-                  <Menu.Button className="-m-2 flex items-center rounded-full p-2 text-gray-500 hover:text-gray-600">
-                    <span className="sr-only">Open options</span>
-                    <DotsHorizontalIcon
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="focus:outline-none absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-                    <div className="py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? 'bg-gray-100 text-gray-900'
-                                : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Uredi
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? 'bg-gray-100 text-gray-900'
-                                : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Prekliči rezervacijo
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </li>
+            <EventListItem reservation={reservation} />
           ))}
         </ol>
       </div>
