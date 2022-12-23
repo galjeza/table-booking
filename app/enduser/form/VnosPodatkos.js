a61c8876ee0ffaa00f542009728e5f77353fca72
Matic Lukežič <31586745+mlukee@users.noreply.github.com>
2022-12-23 19:02:29 +0100

    Add restaurant to form

--- app/enduser/form/VnosPodatkos.js
+++ app/enduser/form/VnosPodatkos.js
@@ -80,6 +80,25 @@ export default function VnosPodatkov({ fields }) {
                 </div>
               </div>
               <div>
+                <label
+                    htmlFor="restaurant"
+                    className="block text-sm font-medium text-gray-700"
+                >
+                  Restavracija
+                </label>
+                <div className="mt-1">
+                  <input
+                      id="restaurant"
+                      name="restaurant"
+                      type="text"
+                      autoComplete="restaurant"
+                      value={fields.naziv}
+                      required
+                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+              <div>
                 <button
                   type="submit"
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
